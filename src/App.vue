e724e649e8dde58b536f78af999d485404b5db2c
cwinters <Cortlandt.Winters@nysed.gov>
2017-10-31 12:03:04 -0400

    add example of dynamic opening of a modal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <div id="app">
     <router-view/>
+
+    <button @click.prevent="openTest">test</button>
+
+    <div id="modalStack"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Vue from 'vue'
+  import TestModal from './modals/TestModal.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      TestModal
+    },
+    methods: {
+      openTest: function() {
+
+        let Constructor = Vue.extend(TestModal);
+
+        new Vue({
+          el: '#modalStack',
+          mounted: function() {
+            let testModal = new Constructor({
+              el: this.$el,
+              parent: this,
+            });
+          },
+        });
+      },
+    }
+  }
 </script>
 
